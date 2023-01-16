72a8e712e490145cb851374135222307d16f888d
Antoni Majkowski <antonimajkowski@macbook-pro-antoni.home>
2023-01-16 21:39:08 +0100

    changed server address

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ class App extends Component {
 
   onImageSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://smart-brain-api-0nuz.onrender.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -73,7 +73,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://smart-brain-api-0nuz.onrender.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
